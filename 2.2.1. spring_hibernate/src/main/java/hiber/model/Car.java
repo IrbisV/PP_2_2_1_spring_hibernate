0e41ddc3cc579be671a3488edcbcf6e94b12477c
Dmitry Vanyukov <ekoetnotur@yandex.ru>
2022-10-02 20:15:39 +0300

    removed the annotation @Column

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -8,13 +8,10 @@ public class Car {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
-    @Column(name = "model")
     private String model;
 
-    @Column(name = "series")
     private int series;
 
     public Car() {
